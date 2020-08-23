ff2f8df5b2d707479df0a3d8c9f5a80c53d869fb
Seth Silesky <seth.silesky@gmail.com>
2020-08-23 18:21:37 -0500

    set up project

---
+++ fixtures/todo-openapi.d.ts
@@ -0,0 +1,119 @@
+export namespace Components {
+    export namespace RequestBodies {
+        export type CreateTodo = Schemas.Todo;
+    }
+    export namespace Responses {
+        export interface CreateTodo {
+            /**
+             * example:
+             * abcdef1234565789
+             */
+            id?: number;
+        }
+        export type GenericError = Schemas.GenericError;
+        export interface GetHelloV1 {
+            /**
+             * example:
+             * Hello, jonathan
+             */
+            greeting?: string;
+        }
+        export type GetTodo = Schemas.Todo;
+        export interface GetTodoList {
+            todos?: Schemas.Todo[];
+        }
+        export interface OpenApiValidationError {
+            message: string;
+            errors: {
+                /**
+                 * example:
+                 * .body.schedule.monday.close
+                 */
+                path: string;
+                /**
+                 * example:
+                 * request.body.schedule.monday should have required property 'close'
+                 */
+                message: string;
+                /**
+                 * example:
+                 * required.openapi.validation
+                 */
+                errorCode: string;
+            }[];
+        }
+    }
+    export namespace Schemas {
+        export interface GenericError {
+            /**
+             * example:
+             * 404
+             */
+            status?: number;
+            /**
+             * example:
+             * Todo not found
+             */
+            message?: string;
+            errors?: {}[];
+        }
+        export interface Todo {
+            id?: number;
+            description: string;
+            category: 'Shopping' | 'Home' | 'Work';
+        }
+    }
+}
+export namespace Paths {
+    export namespace V1Hello {
+        export namespace Get {
+            export namespace Parameters {
+                export type Name = string;
+            }
+            export interface QueryParameters {
+                name: Parameters.Name;
+            }
+            export namespace Responses {
+                export type $200 = Components.Responses.GetHelloV1;
+            }
+        }
+    }
+    export namespace V1Todo {
+        export namespace Get {
+            export namespace Responses {
+                export type $200 = Components.Responses.GetTodoList;
+            }
+        }
+        export namespace Post {
+            export type RequestBody = Components.RequestBodies.CreateTodo;
+            export namespace Responses {
+                export type $201 = Components.Responses.CreateTodo;
+            }
+        }
+    }
+    export namespace V1Todo$Id {
+        export namespace Get {
+            export namespace Parameters {
+                export type Id = number;
+            }
+            export interface PathParameters {
+                id: Parameters.Id;
+            }
+            export namespace Responses {
+                export type $200 = Components.Responses.GetTodo;
+                export type $400 = Components.Responses.OpenApiValidationError;
+                export type $404 = Components.Responses.GenericError;
+            }
+        }
+    }
+    export namespace V2Hello {
+        export namespace Get {
+            export namespace Parameters {
+                export type Foo = boolean;
+            }
+            export interface QueryParameters {
+                foo?: Parameters.Foo;
+            }
+        }
+    }
+}
--- src/post-process.ts
+++ src/post-process.ts
@@ -1,27 +1,28 @@
 import { PluginContext } from 'dtsgenerator';
 import ts from 'typescript';
 
+const transformer = (context: ts.TransformationContext) => (
+    root: ts.SourceFile
+): ts.SourceFile => {
+    function visit(node: ts.Node): ts.Node {
+        node = ts.visitEachChild(node, visit, context);
+        if (ts.isStringLiteral(node)) {
+            // `singleQuote` is internal property.
+            // via: https://github.com/microsoft/TypeScript/blob/v3.9.2/src/compiler/types.ts#L1353
+            ((node as unknown) as {
+                singleQuote: boolean;
+            }).singleQuote = true;
+        }
+
+        return node;
+    }
+    return ts.visitNode(root, visit);
+};
+
 async function postProcess(
     _pluginContext: PluginContext
 ): Promise<ts.TransformerFactory<ts.SourceFile> | undefined> {
-    return (context: ts.TransformationContext) => (
-        root: ts.SourceFile
-    ): ts.SourceFile => {
-        function visit(node: ts.Node): ts.Node {
-            node = ts.visitEachChild(node, visit, context);
-            console.log(node);
-            if (ts.isStringLiteral(node)) {
-                // `singleQuote` is internal property.
-                // via: https://github.com/microsoft/TypeScript/blob/v3.9.2/src/compiler/types.ts#L1353
-                ((node as unknown) as {
-                    singleQuote: boolean;
-                }).singleQuote = true;
-            }
-
-            return node;
-        }
-        return ts.visitNode(root, visit);
-    };
+    return transformer;
 }
 
-export default postProcess
+export default postProcess;

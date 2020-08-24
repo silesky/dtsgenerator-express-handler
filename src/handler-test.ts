faea932c61398785ca27d57cd4b45439ae4b5c63
Seth Silesky <seth.silesky@gmail.com>
2020-08-24 18:28:37 -0500

    add dtsgen

--- src/handler-test.ts
+++ src/handler-test.ts
@@ -1,6 +1,38 @@
 import { RequestHandler } from './types';
 import { Paths } from '../fixtures/todo-openapi';
 
+type Method = {
+    Parameters: Record<string, any>;
+    PathParameters: Record<string, any>;
+    QueryParameters: Record<string, any>;
+    RequestBody: any;
+    Responses: {
+        [key: string]: any;
+    };
+};
+
+export type RequestHandlerWithMethod<M extends Method> = RequestHandler<
+    M['PathParameters'],
+    M['Responses'],
+    M['RequestBody'],
+    M['QueryParameters']
+>;
+export type V1Todo$IdGet = RequestHandlerWithMethod<Paths.V1Todo$Id.Get>;
+
+export const getHelloHandlerV1: RequestHandlerWithMethod<Paths.V1Todo$Id.Get> = async (
+    req,
+    res
+) => {
+    const { name } = req.query;
+    const params = req.params;
+    console.log(params);
+    const greeting = name ? `Hi ${name}!!!!` : 'Hello!!';
+    if (foo) {
+        res.json({ name: 123 });
+    }
+    res.json({ greeting });
+};
+
 export const getHelloHandlerV1: RequestHandler<
     Paths.V1Todo$Id.Get.PathParameters,
     Paths.V1Hello.Get.Responses.$200,
